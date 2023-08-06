54b3d41fc6de1c6f78e25503423cd87daaa64c00
MAC <mac@MACs-MacBook-Air-3.local>
2023-08-06 06:08:41 +0100

    power

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import HomeScreen from './Screens/HomeScreen';
+
+
+
 
 const App = () => {
   return (
-    <h1>Welcome TO Upsurge-Mart</h1>
-  )
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App
---
+++ frontend/src/Components/Rating.jsx
@@ -0,0 +1,27 @@
+import {FaStar, FaStarHalfAlt, FaRegStar} from 'react-icons/fa';
+import React from 'react'
+
+const Rating = ({ value, text}) => {
+  return (
+    <div className='rating'>
+        <span>
+            {value >= 1 ? <FaStar /> : value >= 0.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        </span>
+        <span>
+            {value >= 2 ? <FaStar /> : value >= 1.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        </span>
+        <span>
+            {value >= 3 ? <FaStar /> : value >= 2.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        </span>
+        <span>
+            {value >= 4 ? <FaStar /> : value >= 3.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        </span>
+        <span>
+            {value >= 5 ? <FaStar /> : value >= 4.5 ? <FaStarHalfAlt /> : <FaRegStar />}
+        </span>
+        <span className='ml-2'>{text && text}</span> {/* if text exists, show text */}
+    </div>
+  )
+}
+
+export default Rating
